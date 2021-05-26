24a0087476f329adae09c1d1b71783099fe57a3f
vnovokre <vnovokreschenov@gmail.com>
2021-05-26 16:29:24 +0300

    some changes has been performed

--- app/src/main/java/com/example/paint/MainActivity.java
+++ app/src/main/java/com/example/paint/MainActivity.java
@@ -1,133 +1,126 @@
 package com.example.paint;
 
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Path;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-    DrawingView dv ;
-    private Paint mPaint;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+import com.pes.androidmaterialcolorpickerdialog.ColorPicker;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
+    private Button penButton, eraserButton, penColorButton, backgroundColorButton, clearButton;
+    private TextView taskText;
+    private DrawingView drawingView;
+    private SeekBar penSizeSeekBar, eraserSizeSeekBar;
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dv = new DrawingView(this);
-        setContentView(dv);
-        mPaint = new Paint();
-        mPaint.setAntiAlias(true);
-        mPaint.setDither(true);
-        mPaint.setColor(Color.GREEN);
-        mPaint.setStyle(Paint.Style.STROKE);
-        mPaint.setStrokeJoin(Paint.Join.ROUND);
-        mPaint.setStrokeCap(Paint.Cap.ROUND);
-        mPaint.setStrokeWidth(12);
+        initializeUI();
+        setListeners();
     }
 
-    public class DrawingView extends View {
+    private void setListeners() {
+//        penButton.setOnClickListener(this);
+//        eraserButton.setOnClickListener(this);
+//        penColorButton.setOnClickListener(this);
+//        backgroundColorButton.setOnClickListener(this);
+//        penSizeSeekBar.setOnSeekBarChangeListener(this);
+//        eraserSizeSeekBar.setOnSeekBarChangeListener(this);
+        clearButton.setOnClickListener(this);
+        taskText.setOnClickListener(this);
+    }
 
-        public int width;
-        public  int height;
-        private Bitmap mBitmap;
-        private Canvas mCanvas;
-        private Path mPath;
-        private Paint   mBitmapPaint;
-        Context context;
-        private Paint circlePaint;
-        private Path circlePath;
+    private void initializeUI() {
+        drawingView = findViewById(R.id.scratch_pad);
+//        penButton = findViewById(R.id.pen_button);
+//        eraserButton = findViewById(R.id.eraser_button);
+//        penColorButton = findViewById(R.id.pen_color_button);
+//        backgroundColorButton = findViewById(R.id.background_color_button);
+//        penSizeSeekBar = findViewById(R.id.pen_size_seekbar);
+//        eraserSizeSeekBar = findViewById(R.id.eraser_size_seekbar);
+        clearButton = findViewById(R.id.clear_button);
+        taskText = findViewById(R.id.taskText);
+    }
 
-        public DrawingView(Context c) {
-            super(c);
-            context=c;
-            mPath = new Path();
-            mBitmapPaint = new Paint(Paint.DITHER_FLAG);
-            circlePaint = new Paint();
-            circlePath = new Path();
-            circlePaint.setAntiAlias(true);
-            circlePaint.setColor(Color.BLUE);
-            circlePaint.setStyle(Paint.Style.STROKE);
-            circlePaint.setStrokeJoin(Paint.Join.MITER);
-            circlePaint.setStrokeWidth(4f);
-        }
-
-        @Override
-        protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-            super.onSizeChanged(w, h, oldw, oldh);
-
-            mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-            mCanvas = new Canvas(mBitmap);
-        }
-
-        @Override
-        protected void onDraw(Canvas canvas) {
-            super.onDraw(canvas);
-
-            canvas.drawBitmap( mBitmap, 0, 0, mBitmapPaint);
-            canvas.drawPath( mPath,  mPaint);
-            canvas.drawPath( circlePath,  circlePaint);
-        }
-
-        private float mX, mY;
-        private static final float TOUCH_TOLERANCE = 4;
-
-        private void touch_start(float x, float y) {
-            mPath.reset();
-            mPath.moveTo(x, y);
-            mX = x;
-            mY = y;
-        }
-
-        private void touch_move(float x, float y) {
-            float dx = Math.abs(x - mX);
-            float dy = Math.abs(y - mY);
-            if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
-                mPath.quadTo(mX, mY, (x + mX)/2, (y + mY)/2);
-                mX = x;
-                mY = y;
-
-                circlePath.reset();
-                circlePath.addCircle(mX, mY, 30, Path.Direction.CW);
-            }
-        }
-
-        private void touch_up() {
-            mPath.lineTo(mX, mY);
-            circlePath.reset();
-            // commit the path to our offscreen
-            mCanvas.drawPath(mPath,  mPaint);
-            // kill this so we don't double draw
-            mPath.reset();
-        }
-
-        @Override
-        public boolean onTouchEvent(MotionEvent event) {
-            float x = event.getX();
-            float y = event.getY();
-
-            switch (event.getAction()) {
-                case MotionEvent.ACTION_DOWN:
-                    touch_start(x, y);
-                    invalidate();
-                    break;
-                case MotionEvent.ACTION_MOVE:
-                    touch_move(x, y);
-                    invalidate();
-                    break;
-                case MotionEvent.ACTION_UP:
-                    touch_up();
-                    invalidate();
-                    break;
-            }
-            return true;
+    @Override public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.taskText:
+                taskText.setText(getRandomTask());
+                break;
+//            case R.id.pen_button:
+//                drawingView.initializePen();
+//                break;
+//            case R.id.eraser_button:
+//                drawingView.initializeEraser();
+//                break;
+            case R.id.clear_button:
+                drawingView.clear();
+                break;
+//            case R.id.pen_color_button:
+//                final ColorPicker colorPicker = new ColorPicker(MainActivity.this, 100, 100, 100);
+//                colorPicker.setCallback(
+//                        color -> {
+//                            drawingView.setPenColor(color);
+//                            colorPicker.dismiss();
+//                        });
+//                colorPicker.show();
+//                break;
+//            case  R.id.pen_collor_button:
+//                ColorSheet().colorPicker(
+//                        colors = colors,
+//                        drawingView = { color ->
+//                                // Handle color
+//                        })
+//                        .show(supportFragmentManager)
+//                        break;
+//            case R.id.background_color_button:
+//                final ColorPicker backgroundColorPicker = new ColorPicker(MainActivity.this, 100, 100, 100);
+//                backgroundColorPicker.setCallback(
+//                        color -> {
+//                            drawingView.setBackgroundColor(color);
+//                            backgroundColorPicker.dismiss();
+//                        });
+//                backgroundColorPicker.show();
+//                break;
+            default:
+                break;
         }
     }
 
+    @Override public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+//        int seekBarId = seekBar.getId();
+//        if (seekBarId == R.id.pen_size_seekbar) {
+//            drawingView.setPenSize(i);
+//        } else if (seekBarId == R.id.eraser_size_seekbar) {
+//            drawingView.setEraserSize(i);
+//        }
+    }
+
+    @Override public void onStartTrackingTouch(SeekBar seekBar) {
+        //Intentionally Empty
+    }
+
+    @Override public void onStopTrackingTouch(SeekBar seekBar) {
+        //Intentionally Empty
+    }
+
+    public String getRandomTask(){
+        Random rand = new Random();
+        List<String> tasks = new ArrayList<>();
+        tasks.add("Нарисуй рыбу");
+        tasks.add("Нарисуй подвал");
+        tasks.add("Нарисуй калитку");
+        tasks.add("Нарисуй себя");
+        tasks.add("Нарисуй абырвалг");
+
+        return tasks.get(rand.nextInt(5));
+    }
 
 }
